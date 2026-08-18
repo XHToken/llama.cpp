6498507f528831f26821b7ebb66befa531def744
simple-zheng <1137196813@qq.com>
2026-08-18 17:22:19 +0800

    feat: add Spark3 model and tokenizer support

--- conversion/__init__.py
+++ conversion/__init__.py
@@ -237,6 +237,7 @@ TEXT_MODEL_MAP: dict[str, str] = {
     "SeedOssForCausalLM": "olmo",
     "SmallThinkerForCausalLM": "smallthinker",
     "SmolLM3ForCausalLM": "llama",
+    "Spark3ForCausalLM": "spark3",
     "SolarOpenForCausalLM": "glm",
     "StableLMEpochForCausalLM": "stablelm",
     "StableLmForCausalLM": "stablelm",
